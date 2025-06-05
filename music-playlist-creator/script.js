350f9fdb9359180f5470bcd00c122ab59bba8148
Daniel Palmer <dmpalmer@meta.com>
2025-06-04 17:21:51 -0700

    EOD stash - working on JS implementation and preliminary styling

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,28 @@
+const modal = document.getElementById('playlist-modal');
+const body = document.querySelector('body');
+let isModalOpen = false;
 
+const handleModalOpen = () => {
+    console.log('handle modal open');
+    modal.style.display = 'grid';
+    setTimeout(()=>{
+        isModalOpen = true;
+    },10)
+}
+
+const handleOutsideModalClick = (event) => {
+    console.log('outside modal click');
+    if (isModalOpen){
+        if (!modal.contains(event.target)){
+            modal.style.display = 'none';
+        }
+        isModalOpen = false;
+    }
+}
+document.addEventListener('click', handleOutsideModalClick);
+
+
+const playlists = document.getElementsByClassName('card');
+for (playlist of playlists){
+    playlist.addEventListener('click', handleModalOpen);
+}
